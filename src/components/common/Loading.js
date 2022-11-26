2e723d9475871e91d506235317b03d95b8a9e687
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2022-11-25 21:15:01 -0500

    Update Loading.js

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -4,14 +4,6 @@ const Loading = () => {
   return (
     <div id="loading" className="loading hidden">
       <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
     </div>
   );
