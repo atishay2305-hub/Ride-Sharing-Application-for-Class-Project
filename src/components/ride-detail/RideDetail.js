7a149b864b9bdd468639389d9cbf798c8a9c8c81
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2022-11-25 22:51:57 -0500

    Update RideDetail.js

--- src/components/ride-detail/RideDetail.js
+++ src/components/ride-detail/RideDetail.js
@@ -1,103 +1,73 @@
-import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
+Import React,  useContext  from "react";
+Import  v4 as uuidv4  from "uuid";
 
-import * as firebaseService from "../../services/firebase";
-import * as uiService from "../../services/ui";
+Import * as firebaseService from "../../services/firebase";
+Import * as uiService from "../../services/ui";
 
-import Context from "../../context";
+Import Context from "../../context";
 
-import email from "../../images/email.png";
-import exit from "../../images/exit.png";
-import tick from "../../images/tick.png";
+Const RequestRide = ( toggleModal ) => 
+  const  user, selectedFrom, selectedTo, setRideRequest  =
+    useContext(Context);
 
-const RideDetail = ({ user, isDriver, currentRide }) => {
-  const { setCurrentRide } = useContext(Context);
-
-  const history = useHistory();
-
-  const cancelRide = () => {
-    const isCancel = window.confirm("Do you want to cancel this ride?");
-    if (isCancel) {
-      currentRide.status = "canceled";
-      updateRide(currentRide);
-    }
-  };
-
-  const finishRide = () => {
-    const isFinish = window.confirm("Do you want to finish this ride?");
-    if (isFinish) {
-      currentRide.status = "done";
-      updateRide(currentRide);
-    }
-  };
-
-  const updateRide = async (ride) => {
-    uiService.showLoading();
-    try {
-      await firebaseService.insert({
-        key: "rides",
-        id: ride.rideUuid,
-        payload: ride,
-      });
-    } catch (error) {
-      uiService.hideLoading();
-    }
-    uiService.hideLoading();
-    removeRide();
-  };
-
-  const removeRide = () => {
-    localStorage.removeItem("currentRide");
-    setCurrentRide(null);
-    window.location.reload();
-  };
-
-  const talkToUser = () => {
-    history.push("/chat");
-  };
+  const requestRide = async () => 
+    if (user && selectedFrom && selectedTo) 
+      toggleModal(fake);
+      uiService.ShowLoading();
+      const rideUuid = uuidv4();
+      const journey = 
+        rideUuid: rideUuid,
+        requestor: user,
+        pickup: selectedFrom,
+        destination: selectedTo,
+        reputation: "waiting",
+      ;
+      try 
+        look ahead to firebaseService.Insert(
+          key: "rides",
+          identification: rideUuid,
+          payload: trip,
+        );
+        setRideRequest(journey);
+        uiService.HideLoading();
+       capture (errors) 
+        uiService.HideLoading();
+      
+    
+  ;
 
   return (
-    <div className="ride-detail">
-      <div className="ride-detail__user-avatar">
-        <img src={user.image} alt={user.email} />
-      </div>
-      <div className="ride-detail__actions" onClick={talkToUser}>
-        <div className="ride-detail__action">
-          <img src={email} alt="email" />
-        </div>
-        <div className="ride-detail__action" onClick={cancelRide}>
-          <img src={exit} alt="exit" />
-        </div>
-        {isDriver && (
-          <div className="ride-detail__action" onClick={finishRide}>
-            <img src={tick} alt="tick" />
+    <div className="request-ride">
+      <div className="request-ride__content">
+        <div className="request-ride__container">
+          <div className="request-ride__title">inquiring for a ride</div>
+          <div className="request-ride__close">
+            <img
+              alt="close"
+              onClick=() => toggleModal(fake)
+              src="https://static.Xx.Fbcdn.Net/rsrc.Php/v3/y2/r/__geKiQnSG-.Png"
+            />
           </div>
-        )}
-      </div>
-      <div className="ride-detail__content">
-        <p className="ride-detail__result-label">
-          <span>Email: </span>
-          {user.email}
-        </p>
-        <p className="ride-detail__result-label">
-          <span>Phone: </span>
-          {user.phone}
-        </p>
-        <p className="ride-detail__result-label">
-          <span>From: </span>
-          {currentRide.pickup && currentRide.pickup.label
-            ? currentRide.pickup.label
-            : ""}
-        </p>
-        <p className="ride-detail__result-label">
-          <span>To: </span>
-          {currentRide.destination && currentRide.destination.label
-            ? currentRide.destination.label
-            : ""}
-        </p>
+        </div>
+        <div className="request-ride__subtitle"></div>
+        <div className="request-ride__form">
+          <p>
+            You entered the pickup area efficiently. Do you want to request
+            a ride now ?
+          </p>
+          <button
+            className="request-ride__btn request-ride__change-btn"
+            onClick=() => toggleModal(false)
+          >
+            trade
+          </button>
+          <button className="request-ride__btn" onClick=requestRide>
+            requesting a journey now
+          </button>
+        </div>
       </div>
     </div>
   );
-};
+;
 
-export default RideDetail;
+Export default RequestRide;
